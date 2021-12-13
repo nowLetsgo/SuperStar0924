8f50ef4e519168bd376c0d4a25840908e20a505a
nowLetsgo <lipeihua0922@163.com>
2021-12-13 10:52:54 +0800

    day13-拦截器

---
+++ day13-ajax02/01.API分类.js
@@ -0,0 +1,13 @@
+/* 
+    API的分类
+        1.REST(表现层状态转移) API:    restful
+        (1)发送请求进行CRUD哪个操作由请求方式来决定
+        (2)同一个请求路径可以进行多个操作
+        (3)请求方式会用到GET/POST/PUT/DELETE
+
+        2.非REST API:   restless
+        (1)请求方式不决定请求的CRUD操作
+        (2)一个请求路径只对应一个操作
+        (3)一般只有GET/POST
+
+*/
