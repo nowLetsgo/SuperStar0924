8f69a7390718fd5fb6a09a3e447b4ab733ddaa87
nowLetsgo <lipeihua0922@163.com>
2021-12-13 15:40:06 +0800

    day13-ajax02

---
+++ day13-ajax02/05.跨域请求/02.JsonP跨域/01.server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+//书写一个处理post请求的中间件，把post请求的报文体处理成一个对象，并放在req.body上
+app.use("", express.urlencoded({ //这个是处理form表单格式的报文体的
+    extended: false
+}))
+app.use("", express.json()) //处理json格式的报文体的
+
+app.get("/login", (req, res) => {
+    const {
+        user,
+        pass,
+        callback
+    } = req.query;
+    console.log(user, pass, callback);
+
+    res.set("Content-type", "application/javascript;charset=utf-8")
+
+    if (user === "laoli" && pass === '123456') {
+        return res.send(`${callback}(${JSON.stringify({code:10000,msg:"登录成功"})})`)
+    }
+
+    res.send(`${callback}(${JSON.stringify({code:10001,msg:"登录失败"})})`)
+
+
+
+
+})
+
+app.listen(3000, err => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log("成功， http://192.168.17.62:3000");
+})
