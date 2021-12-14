a166c659a9620d5bd36f5ffba38ed5a6bcddfb6a
nowLetsgo <lipeihua0922@163.com>
2021-12-14 10:39:04 +0800

    day14-登录注册-模块化书写

---
+++ day14-登录注册练习/02.登录注册的模块化/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+//连接数据库模块
+require("./db")
+//引入数据库的用户集合
+const userModel = require("./models/userModel");
+const app = express();
+
+
+//使用express的静态文件中间件，把静态文件暴露出去
+app.use(express.static(path.resolve(__dirname, "./views")))
+app.use('/public', express.static(path.resolve(__dirname, "./public")))
+
+
+app.listen(3000, err => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log("成功，请访问 http://192.168.17.62:3000");
+})
